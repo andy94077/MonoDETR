74f35023ad46f8486533736acc4296162775a91b
andy94077 <andy94077@gmail.com>
2023-02-22 16:49:23 +0800

    Change default model type value

--- lib/helpers/model_helper.py
+++ lib/helpers/model_helper.py
@@ -2,6 +2,6 @@ from lib.models.monodetr import AVAILABLE_MODELS
 
 
 def build_model(model_cfg, loss_cfg):
-    model_type: str = model_cfg.pop('type', 'monodetr')
+    model_type: str = model_cfg.pop('type', 'MonoDETR')
     assert model_type in AVAILABLE_MODELS, f'Invalid model type {model_type}. Supported model types are {list(AVAILABLE_MODELS.keys())}.'
     return AVAILABLE_MODELS[model_type](model_cfg, loss_cfg)

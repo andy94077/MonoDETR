22eeaf4468c76baeb16ea446a8866daa2e6ce24a
andy94077 <andy94077@gmail.com>
2023-02-22 16:19:10 +0800

    Add configs and build functions for monooqdd

--- lib/helpers/model_helper.py
+++ lib/helpers/model_helper.py
@@ -1,5 +1,7 @@
-from lib.models.monodetr import build_monodetr
+from lib.models.monodetr import AVAILABLE_MODELS
 
 
 def build_model(model_cfg, loss_cfg):
-    return build_monodetr(model_cfg, loss_cfg)
+    model_type: str = model_cfg.pop('type', 'monodetr')
+    assert model_type in AVAILABLE_MODELS, f'Invalid model type {model_type}. Supported model types are {list(AVAILABLE_MODELS.keys())}.'
+    return AVAILABLE_MODELS[model_type](model_cfg, loss_cfg)
--- lib/models/monodetr/__init__.py
+++ lib/models/monodetr/__init__.py
@@ -3,13 +3,11 @@ from typing import Any, Dict
 from . import monodetr
 from . import monooqdd
 
-_AVAILABLE_MODELS = {
-    'monodetr': monodetr.build,
-    'monooqdd': monooqdd.build,
+AVAILABLE_MODELS = {
+    'MonoDETR': monodetr.build,
+    'MonoOQDD': monooqdd.build,
 }
 
 
 def build_monodetr(model_cfg: Dict[str, Any], loss_cfg: Dict[str, Any]):
-    model_type: str = model_cfg.pop('type', 'monodetr')
-    assert model_type in _AVAILABLE_MODELS, f'Invalid model type {model_type}. Supported model types are {list(_AVAILABLE_MODELS.keys())}.'
-    return _AVAILABLE_MODELS[model_type](model_cfg, loss_cfg)
+    return monodetr.build(model_cfg, loss_cfg)
